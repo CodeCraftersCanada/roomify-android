56b171927e25d6a058a046e49013f5a5c994c53a
Stainley Lebron <stainley.lebron@gmail.com>
2023-12-05 23:56:36 -0500

    Some screens for Listing Properties

---
+++ app/src/main/java/edu/lambton/roomify/landlord/view/ConfirmAddressActivity.java
@@ -0,0 +1,16 @@
+package edu.lambton.roomify.landlord.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.lambton.roomify.R;
+
+public class ConfirmAddressActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_confirm_address);
+    }
+}
