4b64f9fe29d9da936c0d1f343fd961107c4cbee8
Elvin Ross Fabella <elvinross.fabella@gmail.com>
2023-12-06 21:40:58 -0500

    add steps 1.a to 1.c

--- app/src/main/java/edu/lambton/roomify/landlord/view/listing/GetStartedListPropertyActivity.java
+++ app/src/main/java/edu/lambton/roomify/landlord/view/listing/GetStartedListPropertyActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import edu.lambton.roomify.R;
 
-public class GetStartListPropertyActivity extends AppCompatActivity {
+public class GetStartedListPropertyActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_get_start_list_property);
+        setContentView(R.layout.activity_get_started_list_property);
     }
 }
