64c4a43b34531289a6ceaa788a0553d99eef3e4a
Elvin Ross Fabella <elvinross.fabella@gmail.com>
2023-12-06 21:35:22 -0500

    add manifest

---
+++ app/src/main/java/edu/lambton/roomify/landlord/view/listing/GetStartedListPropertyActivity.java
@@ -0,0 +1,16 @@
+package edu.lambton.roomify.landlord.view.listing;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import edu.lambton.roomify.R;
+
+public class GetStartListPropertyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_start_list_property);
+    }
+}
